d52be70f338b8e01a1bfac30269f093c23fc3de6
arayyan0 <arayyan@physics.utoronto.ca>
2020-12-17 14:45:33 -0500

    updated with f strings

--- sweeps/kg_a_sweep.py
+++ sweeps/kg_a_sweep.py
@@ -5,14 +5,12 @@
 
 import numpy as np
 import sys
-from numpy import linspace
 import os
 
 args = sys.argv
-type, sub, l1, l2, Tf_pow, MS_pow, DS_pow, versions = [int(x) for x in args[1:9]]
-cluster = input('WARNING: You better have picked the right cluster!\nInput one of RH/RE, and one of 1/2: ')
+hc_or_kek, type, sub, l1, l2, Tf_pow, MS_pow, DS_pow, versions = [int(x) for x in args[1:10]]
 run=int(args[10])
-command = './sim %i %i %i %i %i %i'%(sub, l1, l2, Tf_pow, MS_pow, DS_pow)
+command = f"./sim {hc_or_kek} {type} {sub} {l1} {l2} {Tf_pow} {MS_pow} {DS_pow}"
 
 # g = 0.5
 # a = float(args[9])
@@ -58,54 +56,44 @@ command = './sim %i %i %i %i %i %i'%(sub, l1, l2, Tf_pow, MS_pow, DS_pow)
 # F.write('parallel --jobs 80 --joblog g_%.3f_a_%.3f.out < g_%.3f_a_%.3f.lst'%(g,a,g,a)+'\n')
 # F.close()
 
+p = float(args[11])
+print(f"p: {p}")
 g = 0.5
-p = float(args[9])
-print("p:")
-print(p)
-# alst = linspace(-0.5, 1, 30+1)
-# #####
-# alst = linspace(0.4,0.65,25+1)
-# #####
-# alst = linspace(-0.03,0.21,80+1) #80+1 or 24+1
-# alst = linspace(0.1,0.5, ) #80+1 or 25+1
-# alst = linspace(0.4,0.8, ) #80+1 or 25+1
-# alst = linspace(0,0.4,20+1)
-# alst = np.array([0.00,0.10,0.45,0.60])
-# alst = np.array([0.999])
-alst = [0.8]
+alst = np.linspace(-0.4, 0, 80+1)
+# alst = np.linspace(0.005, 0.805, 80+1)
 print("a:")
 print(alst)
 
-path = 'out/g_%.3f_p_%.3f_r_%i'%(g,p,run)
+path = f'out/p_{p:.3f}_r_{run}'
 if not os.path.exists(path):
   os.makedirs(path)
 
 F = open(path+"/SA_param.lst",'w+')
-F.write("type: %i\n"%type)
-F.write("(s, l1, l2) = (%i, %i, %i)\n"%(sub, l1, l2))
-F.write("Tf = %.20f\n"%(0.9)**Tf_pow)
-F.write("Metropolis sweeps = %i\n"%(10)**MS_pow)
-F.write("Deterministic sweeps = %i\n"%(10)**DS_pow)
-F.write("Cluster: %s\n"%cluster)
+F.write(f"hc or kekule: {hc_or_kek}\n")
+F.write(f"type: {type}\n")
+F.write(f"(s, l1, l2) = ({sub}, {l1}, {l2})\n")
+F.write(f"Tf = {(0.9)**Tf_pow:.20f}\n")
+F.write(f"Metropolis sweeps = {(10)**MS_pow}\n")
+F.write(f"Deterministic sweeps = {(10)**DS_pow}}\n")
 F.close()
 
-F = open('g_%.3f_p_%.3f_r_%i.lst'%(g,p,run),'w+')
+F = open(f'p_{p:.3f}_r_{run}.lst','w+')
 for v in range(1, versions+1):
-    if not os.path.exists(path+'/v_%i'%v):
-      os.makedirs(path+'/v_%i'%v)
+    if not os.path.exists(path+f'/v_{v}'):
+      os.makedirs(path+f'/v_{v}')
     for a in alst:
-        F.write(command+' %.3f %.3f %.3f'%(p,g,a) + ' > ' + path+'/v_%i/a_%.3f_.out\n'%(v,a))
+        F.write(command+f' {p:.3f} {g:.3f} {a:.3f}' + ' > ' + path+f'/v_{v}/a_{a:.3f}_.out\n')
 F.close()
 #
-F = open('g_%.3f_p_%.3f_r_%i.sh'%(g,p,run),'w+')
-F.write('#!/bin/bash'+'\n'+'\n')
-F.write('#SBATCH --nodes=1'+'\n')
-F.write('#SBATCH --cpus-per-task=80'+'\n')
-F.write('#SBATCH --time=00:30:00'+'\n')
-F.write('#SBATCH --job-name=g_%.3f_p_%.3f_r_%i'%(g,p,run)+'\n'+'\n')
-F.write('cd $SLURM_SUBMIT_DIR'+'\n'+'\n')
-F.write('module purge'+'\n')
-F.write('module load gcc'+'\n')
-F.write('module load gnu-parallel'+'\n')
-F.write('parallel --jobs 80 --joblog out/g_%.3f_p_%.3f_r_%i/g_%.3f_p_%.3f_r_%i.out < g_%.3f_p_%.3f_r_%i.lst'%(g,p,run,g,p,run,g,p,run)+'\n')
+F = openf(f'p_{p:.3f}_r_{run}.sh','w+')
+F.write(f'#!/bin/bash'+'\n'+'\n')
+F.write(f'#SBATCH --nodes=1'+'\n')
+F.write(f'#SBATCH --cpus-per-task=80'+'\n')
+F.write(f'#SBATCH --time=00:30:00'+'\n')
+F.write(f'#SBATCH --job-name=p_{p}_r_{run}'+'\n'+'\n')
+F.write(f'cd $SLURM_SUBMIT_DIR'+'\n'+'\n')
+F.write(f'module purge'+'\n')
+F.write(f'module load gcc'+'\n')
+F.write(f'module load gnu-parallel'+'\n')
+F.write(f'parallel --jobs 80 --joblog out/p_{p:.3f}_r_{i}/p_{p:.3f}_r_{i}.out < p_{p:.3f}_r_{i}.lst'+'\n')
 F.close()
