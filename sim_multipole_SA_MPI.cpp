644cbf1d4ffa3ce9442b237bdc5a2b9251e02629
arayyan0 <a.rayyan@mail.utoronto.ca>
2021-07-20 18:37:59 -0400

    removing simulated annealing from finite T step defect length is tunable from command line

--- sim_multipole_SA_MPI.cpp
+++ sim_multipole_SA_MPI.cpp
@@ -26,18 +26,20 @@ int main(int argc, char *argv[])
   const long double lambda = 0.0;
   const long double ising_y = strtod(argv[4], NULL);
   const long double defect = strtod(argv[5], NULL);
+  const long double defect_lengthscale = strtod(argv[6], NULL);
   const long double h_field = 0.000;
   Vector3LD hdir = Vector3LD(0,1,0).normalized();
 
   //simulated annealing settings
-  const uint num_SA_steps = strtol(argv[6], NULL, 10);
-  const uint num_sweeps_SA = pow(10, strtol(argv[7], NULL, 10));
+  const uint num_SA_steps = strtol(argv[7], NULL, 10);
+  const uint num_sweeps_SA = pow(10, strtol(argv[8], NULL, 10));
   double cooling_rate = 0.9;
   double initial_T = 1*abs(jtau);
   double final_T = pow(cooling_rate,num_SA_steps);
 
   //deterministic sweep settings
-  const uint max_det_sweeps = pow(10,strtol(argv[8], NULL, 10));
+  const uint max_det_sweeps = pow(10,strtol(argv[9], NULL, 10));
+  // const uint max_det_sweeps = 1;
 
   //no thermalization or measurements in pure simulated annealing
   const uint num_sweeps_thermal = 0;
@@ -55,7 +57,7 @@ int main(int argc, char *argv[])
   long double* minarr = (long double *)malloc(sizeof(long double)*size);
 
   //initiate simulation on each process
-  TriangularLattice triangular(l1, l2, num_defects, jtau, lambda, ising_y, defect, h_field, hdir);
+  TriangularLattice triangular(l1, l2, num_defects, jtau, lambda, ising_y, defect, defect_lengthscale, h_field, hdir);
   triangular.SimulatedAnnealing(num_sweeps_SA, initial_T, final_T,cooling_rate);
   triangular.DeterministicSweeps(max_det_sweeps);
 
@@ -76,19 +78,19 @@ int main(int argc, char *argv[])
                                              initial_T, final_T, num_sweeps_SA,
                                              num_sweeps_thermal, num_sweeps_measurement,
                                              sampling_time, max_det_sweeps);
-    which << "------------------------Hamiltonian Parameters------------------------\n";
-    which << "J_Tau\n";
-    which << jtau << "\n";
-    which << "Lambda\n";
-    which << lambda << "\n";
-    which << "IsingY\n";
-    which << ising_y << "\n";
-    which << "defect/number of defects\n";
-    which << defect << "/" << num_defects<< "\n";
-    which << "HField\n";
-    which << h_field << "\n";
-    which << "HDirection\n";
-    which << hdir.transpose() << "\n";
+   which << "------------------------Hamiltonian Parameters------------------------\n";
+   which << "J_Tau\n";
+   which << jtau << "\n";
+   which << "Lambda\n";
+   which << lambda << "\n";
+   which << "IsingY\n";
+   which << ising_y << "\n";
+   which << "defect/length scale/number of defects\n";
+   which << defect << "/" << defect_lengthscale << "/" <<num_defects<< "\n";
+   which << "HField\n";
+   which << h_field << "\n";
+   which << "HDirection\n";
+   which << hdir.transpose() << "\n";
     triangular.PrintConfiguration(which);
   }
   MPI_Finalize();
--- sim_multipole_finiteT.cpp
+++ sim_multipole_finiteT.cpp
@@ -25,54 +25,65 @@ int main(int argc, char *argv[])
   const long double lambda = 0;
   const long double ising_y = strtod(argv[4], NULL);
   const long double defect = strtod(argv[5], NULL);
+  const long double defect_lengthscale = strtod(argv[6], NULL);
   const long double h_field = 0.000;
   Vector3LD hdir = Vector3LD(0,0,1).normalized();
   // cout << l1<<l2<<ising_y<<defect<<endl;
-  TriangularLattice triangular(l1, l2, num_defects,jtau, lambda, ising_y, defect, h_field, hdir);
+  TriangularLattice triangular(l1, l2, num_defects,jtau, lambda, ising_y, defect, defect_lengthscale, h_field, hdir);
 
-  const uint num_sweeps_SA = 1e2;
-  const uint num_SA_steps = 60;
-  double cooling_rate = 0.9;
-  double final_T = strtod(argv[6], NULL)*abs(jtau);
-  double critical_T = 0.75*abs(jtau); //depends on the system and size!!
-  double potential_initial_T = final_T/pow(cooling_rate,num_SA_steps-1);
-  double initial_T = potential_initial_T > critical_T ? potential_initial_T : critical_T ;
-  triangular.SimulatedAnnealing(num_sweeps_SA, initial_T, final_T,cooling_rate);
+  double final_T = strtod(argv[7], NULL)*abs(jtau);
 
-  const uint num_sweeps_thermal = 1*1e3;
-  triangular.ThermalizeConfiguration(final_T, num_sweeps_thermal);
-
-  const uint sampling_time = 1e2;
-  const uint num_sweeps_measurement = 0*(1e4)*sampling_time;
-  triangular.SampleConfiguration(final_T, num_sweeps_measurement, sampling_time);
-
-  double actual_det_sweeps=0;
-
-  // std::ostream &which = std::cout;
-  // which << std::fixed << std::setprecision(14);
-  // PrintTriangularSimulationData(which, type, sublattice, l1, l2,
-                                           // initial_T, final_T, num_sweeps_SA,
-                                           // num_sweeps_thermal,num_sweeps_measurement,
-                                           // sampling_time, actual_det_sweeps);
-  // which << "------------------------Hamiltonian Parameters------------------------\n";
-  // which << "J_Tau\n";
-  // which << jtau << "\n";
-  // which << "Lambda\n";
-  // which << lambda << "\n";
-  // which << "IsingY\n";
-  // which << ising_y << "\n";
-  // which << "defect/number of defects\n";
-  // which << defect << "/" << num_defects<< "\n";
-  // which << "HField\n";
-  // which << h_field << "\n";
-  // which << "HDirection\n";
-  // which << hdir.transpose() << "\n";
-  // triangular.PrintConfiguration(which);
+  uint num_sweeps_SA, num_sweeps_thermal;
+  double initial_T, cooling_rate;
+  // double abovecritical_T = 3*abs(jtau);
+  // if (final_T > abovecritical_T){
+  //   num_sweeps_SA = 0;
+  //   initial_T = 0;
+  //   //skip simulated annealing and do extra long thermalization instead
+  //   num_sweeps_thermal = 1e5;
+  // } else {
+  //   num_sweeps_SA = 1*1e3;
+  //   initial_T = abovecritical_T;
+  //   cooling_rate = 0.9;
+  //   triangular.SimulatedAnnealing(num_sweeps_SA, initial_T, final_T, cooling_rate);
   //
-  // triangular.PrintThermalObservables(which);
+  //   num_sweeps_thermal = 1e4;
+  // }
+  num_sweeps_SA = 0;
+  initial_T = 0;
+  num_sweeps_thermal = 5*1e4;
+  triangular.ThermalizeConfiguration(final_T, num_sweeps_thermal);
+  //
+  const uint sampling_time = 1e2;
+  const uint num_sweeps_measurement = 1*(1e4)*sampling_time;
+  triangular.SampleConfiguration(final_T, num_sweeps_measurement, sampling_time);
+  //
+  double actual_det_sweeps=0;
+  std::ostream &which = std::cout;
+  which << std::fixed << std::setprecision(14);
+  PrintTriangularSimulationData(which, type, sublattice, l1, l2,
+                                           initial_T, final_T, num_sweeps_SA,
+                                           num_sweeps_thermal,num_sweeps_measurement,
+                                           sampling_time, actual_det_sweeps);
+  which << "------------------------Hamiltonian Parameters------------------------\n";
+  which << "J_Tau\n";
+  which << jtau << "\n";
+  which << "Lambda\n";
+  which << lambda << "\n";
+  which << "IsingY\n";
+  which << ising_y << "\n";
+  which << "defect/length scale/number of defects\n";
+  which << defect << "/" << defect_lengthscale << "/" <<num_defects<< "\n";
+  which << "HField\n";
+  which << h_field << "\n";
+  which << "HDirection\n";
+  which << hdir.transpose() << "\n";
+  triangular.PrintConfiguration(which);
+
+  triangular.PrintThermalObservables(which);
 
 
-  // prints nearest neighbours of each site
+  // // prints nearest neighbours of each site
   // for (int y=0; y<l2; ++y)
   // {
   //   for (int x=0; x<l1; ++x)
