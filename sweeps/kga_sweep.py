e6c4ff0a39482519c8fd6b6a473292ec043c8c12
arayyan0 <a.rayyan@mail.utoronto.ca>
2021-08-18 15:44:38 -0400

    added tasks per node and mpi annealing options

--- sweeps/kga_sweep.py
+++ sweeps/kga_sweep.py
@@ -1,16 +1,20 @@
 from sweep_lib import KGammaAnisotropyJobs
 
 # min, max, spacing
-p_val_list = [0.1, 0.2, 0.01, 'p']
-a_val_list = [0.02, 0.02, 0.02, 'a']
+p_val_list = [0.073, 0.091, 0.002, 'p']
+a_val_list = [0.0, 0.0, 0.01, 'a']
 
 # hc_or_kek, type, s, l1, l2
-cluster_list = [0, 2, 6, 1, 1]
+cluster_list = [0, 2, 2, 1, 2]
 
 # Tf, MS_pow, DS_pow
-param_list = [200, 4, 4]
+param_list = [200, 5, 4]
 
-run = 3
-versions = 1
+#ensure that num_anneal evenly divides cpus_per_task
+cpus_per_task = 80
+num_anneal = 16
 
-KGammaAnisotropyJobs('p', p_val_list, a_val_list, cluster_list, param_list, run, versions)
+run = 1
+
+KGammaAnisotropyJobs('p', p_val_list, a_val_list, cluster_list, param_list,
+                     cpus_per_task, num_anneal, run)
