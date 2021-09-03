bb4ab9dbde5360c7d0d68cdc21550a737c57e01e
arayyan0 <a.rayyan@mail.utoronto.ca>
2021-09-03 00:23:25 -0700

    added symmetry points

--- common.hpp
+++ common.hpp
@@ -8,6 +8,7 @@
 #include <array>
 #include <chrono>
 #include <cmath>
+#include <complex>
 #include "Eigen/Dense"
 #include <fstream>
 #include <iomanip>
@@ -24,6 +25,7 @@ typedef unsigned int uint;
 
 typedef Eigen::Matrix<long double, 2, 1> Vector2LD;
 typedef Eigen::Matrix<long double, 3, 1> Vector3LD;
+typedef Eigen::Matrix<std::complex<long double>, 3, 1> Vector3LDc;
 typedef Eigen::Matrix<long double, 3, 3> Matrix3LD;
 
 typedef Eigen::Array<long double, Eigen::Dynamic, 1> ArrayXLD;
@@ -38,6 +40,19 @@ typedef Eigen::Array<long double, Eigen::Dynamic, Eigen::Dynamic> ArrayXXLD;
 #define LATTICE_DIR const Vector2LD a1 = Vector2LD( 1.0/2.0, +sqrt(3)/2.0);\
                     const Vector2LD a2 = Vector2LD(-1.0/2.0, +sqrt(3)/2.0);
 
+#define RECIP_DIR const Vector2LD b1 = 2*pi*Vector2LD( 1.0, 1/sqrt(3));\
+                  const Vector2LD b2 = 2*pi*Vector2LD(-1.0, 1/sqrt(3));
+
+#define SYM_DIR const Vector2LD   gamma = 0.0*(b1+b2);\
+                const Vector2LD gammapx = b1;\
+                const Vector2LD gammapy = b2;\
+                const Vector2LD gammapz = b1+b2;\
+                const Vector2LD      mx = b1/2.0;\
+                const Vector2LD      my = b2/2.0;\
+                const Vector2LD      mz = (b1+b2)/2.0;\
+                const Vector2LD      kx = (b1+2.0*b2)/3.0;\
+                const Vector2LD      ky = (2.0*b1+b2)/3.0;\
+                const Vector2LD      kz = (b1-b2)/3.0;
 
 void BravaisIndicesToFlat(const uint& x, const uint&y, const uint & period1,
                                          const uint&z, const uint & period2,
