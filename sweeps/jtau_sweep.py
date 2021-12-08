0ff298fd2d308fe52faa915fca447f0507214fd2
arayyan0 <a.rayyan@mail.utoronto.ca>
2021-12-08 15:32:34 -0500

    preloaded jobrun2

--- sweeps/jtau_sweep.py
+++ sweeps/jtau_sweep.py
@@ -2,14 +2,14 @@ from sweep1_lib import SweepPhaseDiagramJobs
 import numpy as np
 
 # lattice, shape, l1, l2, l3
-cluster_list = [1, 2, 2, 2, 1]
+cluster_list = [1, 1, 6, 6, 1]
 
 # min, max, spacing
 entry = 0
 if entry == 0:
     #jtau2 + jq2 + jo2 = 1
-    t_val_list,  t_label  = [-0.040, 0.040, 0.002], "t"
-    p_val_list,  p_label  = [ 0.550, 0.550, 0.001], "p"
+    t_val_list,  t_label  = [ 0.00625, 0.500, 0.00625], "t"
+    p_val_list,  p_label  = [ 0.500, 0.500, 0.001], "p"
     params_list = [t_val_list, p_val_list]
     params_label_list = [t_label, p_label]
 
@@ -20,7 +20,7 @@ elif entry == 1:
     params_list = [jq_val_list, jo_val_list]
     params_label_list = [jq_label, jo_label]
 
-jb_val_list, jb_label = [0.000, 0.000, 0.001], "jb"
+jb_val_list, jb_label = [np.sqrt(2.0/5.0), np.sqrt(2.0/5.0), 0.001], "jb"
 h_val_list,  h_label  = [0.000, 0.000, 0.001], "h"
 
 params_list = params_list + [jb_val_list, h_val_list]
@@ -31,13 +31,13 @@ params_label_list = params_label_list + [jb_label,h_label]
 # defect_list = [0, 0, 1]
 
 # Tf, MS_pow, DS_pow
-sa_list = [200, 4, 4]
+sa_list = [100, 5, 4]
 
 #ensure that num_anneal divides cpus_per_task
-cpus_per_task = 6
-num_anneal = 6
+cpus_per_task = 80
+num_anneal = 10
 
-run = 6
+run = 2
 versions = 1
 
 SweepPhaseDiagramJobs(cluster_list, params_list, params_label_list, sa_list,
